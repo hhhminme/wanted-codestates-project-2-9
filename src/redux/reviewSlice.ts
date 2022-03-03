c3847958bd9a6cbfd83a2b1f76ce08443d5e8938
Leo-Xee <jang.xmin@gmail.com>
2022-03-03 23:17:24 +0900

    Feat: MockData 위치 변경

--- src/redux/reviewSlice.ts
+++ src/redux/reviewSlice.ts
@@ -1,27 +1,13 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { MOCK_DATA } from "./data";
+
+import { Review } from "../assets/data";
+import data from "../assets/data";
 // type Opaque<T, K extends string> = T & { __typename: K };
 // type Base64 = Opaque<string, "base64">;
 
-interface Review {
-  id: string;
-  productNm: string;
-  productImg: string;
-  createDt: string;
-  review: string;
-  reviewRate: number;
-  likeCnt: number;
-  comments: Comment[];
-}
-
-type Comment = {
-  commentId: string;
-  content: string;
-};
-
 export const reviewSlice = createSlice({
   name: "reviews",
-  initialState: MOCK_DATA as Review[],
+  initialState: data as Review[],
   reducers: {
     add: (state: Review[], action: PayloadAction<Review>) => {
       state.push(action.payload);
