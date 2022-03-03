1d447f86244e28165fc1a02613a3456e986a6d03
hoyong <wjsghdyd4538@yonsei.ac.kr>
2022-03-03 16:31:15 +0900

    feat : 글로벌 네이게이션 바 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 import ReviewList from "./pages/ReviewList";
 import ReviewDetails from "./pages/ReviewDetails";
 import ReviewRegister from "./pages/ReviewRegister";
 
+import GlobalNavigation from "./components/GlobalNavigation";
+
+const GlobalContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: white;
+`;
+
 function App() {
   return (
-    <>
+    <GlobalContainer>
       <Router>
+        <GlobalNavigation />
         <Routes>
           <Route path="/" element={<ReviewList />} />
           <Route path="/details/:id" element={<ReviewDetails />} />
           <Route path="/register" element={<ReviewRegister />} />
         </Routes>
       </Router>
-    </>
+    </GlobalContainer>
   );
 }
 
--- src/pages/ReviewList/ReviewList.tsx
+++ src/pages/ReviewList/ReviewList.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import * as S from "./style";
 
 function ReviewList() {
-  return <div>ReviewList</div>;
+  return <div>list</div>;
 }
 
 export default ReviewList;
