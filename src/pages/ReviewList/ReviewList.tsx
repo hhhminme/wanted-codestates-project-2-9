973a90692e01fdda3e23f019b2ac790ddbbe1b00
hoyong <wjsghdyd4538@yonsei.ac.kr>
2022-03-03 17:54:24 +0900

    feat : tab 구현

--- src/pages/ReviewList/ReviewList.tsx
+++ src/pages/ReviewList/ReviewList.tsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsGrid3X3, BsViewList } from "react-icons/bs";
+import ViewTab from "src/components/ViewTab";
 import * as S from "./style";
 
+const tabData = [
+  { id: 1, name: "grid", icon: BsGrid3X3 },
+  { id: 2, name: "list", icon: BsViewList },
+];
+
 function ReviewList() {
-  return <div>list</div>;
+  const [viewType, setViewType] = useState("grid");
+
+  return (
+    <div>
+      <ViewTab data={tabData} tabHandler={(type: string) => setViewType(type)} />
+      {viewType === "grid" && (
+        <S.GridViewContainer>
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+          <img src="https://dummyimage.com/130x130.jpg" alt="" />
+        </S.GridViewContainer>
+      )}
+      {viewType === "list" && <div>리스트 뷰 데이터 뿌려</div>}
+    </div>
+  );
 }
 
 export default ReviewList;
--- src/pages/ReviewList/style.ts
+++ src/pages/ReviewList/style.ts
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
-const TabBlock = styled.section`
-  width: 100%;
+export const GridViewContainer = styled.section`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  align-items: center;
 `;
