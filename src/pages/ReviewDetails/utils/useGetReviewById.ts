9020c4bec793f8ba9a20f8f20b56faa38129ce47
jinn2u <bsybear623@gmail.com>
2022-03-03 20:07:42 +0900

    feat: store에서 아이디에 해당하는 리뷰를 가져오는 훅

---
+++ src/pages/ReviewDetails/utils/useGetReviewById.ts
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { RootState } from "src/redux/store";
+
+export const useGetReviewById = () => {
+  const { data: reviews } = useSelector((store: RootState) => store.reviews);
+  const { id: reviewId } = useParams();
+
+  return reviews.filter(({ id }) => id === reviewId)[0];
+};
