86a875ad33e33e586be9412340014b1b3a382a98
Leo-Xee <jang.xmin@gmail.com>
2022-03-03 17:17:17 +0900

    feat: 이미지 업로드

--- src/pages/ReviewRegister/ReviewRegister.tsx
+++ src/pages/ReviewRegister/ReviewRegister.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 function ReviewDetails() {
-  return <div>ReviewRegister</div>;
+  const [images, setImages] = useState([]);
+  const imageInput = useRef<HTMLInputElement>(null);
+
+  const onClickImage = () => {
+    imageInput.current?.click();
+  };
+
+  const onChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setImages([...e.target.files]);
+  };
+
+  return (
+    <div>
+      <form>
+        <label>
+          제목
+          <input type="text"></input>
+        </label>
+        <label>
+          내용
+          <input type="textarea" />
+        </label>
+        <input type="file" onChange={onChangeImage} ref={imageInput} multiple hidden />
+        <button onClick={onClickImage}>이미지 업로드</button>
+        <button type="submit">리뷰 등록하기</button>
+      </form>
+    </div>
+  );
 }
 
 export default ReviewDetails;
