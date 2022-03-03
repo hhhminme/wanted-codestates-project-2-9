5d0ec4447e292f61624188b561a9a2a39283b8ec
jinn2u <bsybear623@gmail.com>
2022-03-03 20:08:03 +0900

    feat: review-detail 마크업

--- src/pages/ReviewDetails/index.tsx
+++ src/pages/ReviewDetails/index.tsx
@@ -1,1 +1,29 @@
-export { default } from "./ReviewDetails";
+import React from "react";
+import { FiThumbsUp, FiPaperclip } from "react-icons/fi";
+import { MdComment } from "react-icons/md";
+
+import { useGetReviewById } from "./utils/useGetReviewById";
+import * as S from "./style";
+
+const ReviewDetails = () => {
+  const { productImg, likeCnt, productNm, review } = useGetReviewById();
+  return (
+    <S.Wrapper>
+      <S.Img src={productImg} />
+      <S.Mid1>
+        <S.Section>
+          <FiThumbsUp size={20} />
+          <S.H3>{likeCnt}</S.H3>
+        </S.Section>
+        <S.Section>
+          <MdComment size={20} />
+          <FiPaperclip size={20} />
+        </S.Section>
+      </S.Mid1>
+      <S.H1>{productNm}</S.H1>
+      <S.P>{review}</S.P>
+    </S.Wrapper>
+  );
+};
+
+export default ReviewDetails;
---
+++ src/pages/ReviewDetails/style.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 390px;
+  margin: 0 auto;
+  padding: 0px 50px;
+  display: flex;
+  flex-direction: column;
+  /* background-color: orange; */
+  gap: 15px;
+`;
+export const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+`;
+export const Img = styled.img`
+  object-fit: contain;
+  width: 390px;
+  height: 500px;
+  border-radius: 5px;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+`;
+export const Mid1 = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+export const H3 = styled.h2`
+  font-size: 1.5rem;
+`;
+export const Section = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+export const H1 = styled.h1`
+  font-size: 2rem;
+`;
+export const P = styled.p`
+  font-size: 1rem;
+  padding: 5px;
+  border-radius: 5px;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+  margin-bottom: 15px;
+`;
